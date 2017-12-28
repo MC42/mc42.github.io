3f8e551cd52ed050858f1a3b0e7f8ffbcd46f766
Tim Flynn <tjflynn@theflynn.org>
2017-12-27 19:23:09 -0500

    Kickoff Bingo

---
+++ bingo/js/data.js
@@ -0,0 +1,54 @@
+
+var headerText = "FRC Kickoff Bingo";
+
+var footerText = "";
+
+var winText = "Winner";
+
+var clickSnd = new Audio("audio/click.mp3");
+
+var winSnd = new Audio("audio/win.mp3");
+
+var JSONBingo = {"squares": [
+{"square": "Water Game"},
+{"square": "Dean Speech"},
+{"square": "Woodie GP"},
+{"square": "\"Apply Apply Apply!\""},
+{"square": "Bad acting"},
+{"square": "Don Bossi dressed up"},
+{"square": "Scene at Westwind"},
+{"square": "Dean's List Cameo"},
+{"square": "Poorly Greenscreened Guest"},
+{"square": "\"Play For Real\""},
+{"square": "FIRST Vets Crying"},
+{"square": "Dozerless Game Animation"},
+{"square": "Special Performance by [artist]"},
+{"square": "Corny 8-Bit Art"},
+{"square": "Twitch Fails Spectacularly"},
+{"square": "\"Technical Difficulty\""},
+{"square": "Founding Sponsors Roll"},
+{"square": "Four Year Game Theory Confirmed Again"},
+{"square": "#MakeItLoud"},
+{"square": "\"you are the future\""},
+{"square": "#STEMSquad"},
+{"square": "representive from white house"},
+{"square": "hello to new events"},
+{"square": "2 champs reference "},
+{"square": "FoC reference"},
+{"square": "no safety glasses on field tour"},
+{"square": "over use of denim "},
+{"square": "new power up logo after kickoff"},
+{"square": "Michelle Long talking fast"},
+{"square": "old game elements make cameo"},
+{"square": "FIRST website crashes"},
+{"square": "over 7 minutes of talking in 1 scene"},
+{"square": "plead for alumni to come back and give back"},
+{"square": "AndyMark is the only seller of game piece"},
+{"square": "no hard cap on max score"},
+{"square": "people reading the manual without being told"},
+{"square": "Coopertition FIRST"},
+{"square": "Climbing Endgame"}   	
+    ]
+};
+
+
